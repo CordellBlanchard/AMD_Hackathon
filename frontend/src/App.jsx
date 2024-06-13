d71123446bdd60447a089ab21536b090325cc87e
Zhu <kaizhu12@amd.com>
2024-06-13 05:21:33 -0400

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import IssuesList from './components/IssuesList'
+
+function App() {
+
+  return (
+    <>
+      <h1>CodeQL Detective</h1>
+  
+      <IssuesList />
+    </>
+  )
+}
+
+export default App
